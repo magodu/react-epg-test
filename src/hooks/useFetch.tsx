68e5dedf200e401dd514ab915841065593c6a165
Mario Gonzalez Duarte <magodu.pral@gmail.com>
2023-03-16 15:11:10 +0100

    remove unused hook

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const useFetch = (url: string ) => {
     const [response, setResponse] = useState<any>('');
