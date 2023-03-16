ac2afbe0e0c2ff17673e763c23d99b8fad89a492
Mario Gonzalez Duarte <magodu.pral@gmail.com>
2023-03-16 12:40:26 +0100

    performance: lazy loading

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,4 +1,31 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
+
+import Spinner from './components/UI/Spinner/Spinner';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Guide = lazy(() => import('./pages/Guide/Guide'));
+const OnDemand = lazy(() => import('./pages/OnDemand/OnDemand'));
+const WatchAgain = lazy(() => import('./pages/WatchAgain/WatchAgain'));
+const Favorites = lazy(() => import('./pages/Favorites/Favorites'));
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate replace to="/guide" />} />
+            <Route path="/home" element={<Suspense fallback={<Spinner />}><Home /></Suspense>} />
+            <Route path="/on-demand" element={<Suspense fallback={<Spinner />}><OnDemand /></Suspense>}  />
+            <Route path="/guide" element={<Suspense fallback={<Spinner />}><Guide /></Suspense>} />
+            <Route path="/watch-again" element={<Suspense fallback={<Spinner />}><WatchAgain /></Suspense>} />
+            <Route path="/favorites" element={<Suspense fallback={<Spinner />}><Favorites /></Suspense>}  />
+        </Routes>
+    );
+};
+
+export default AppRoutes;
+
+
+/* import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
 import OnDemand from './pages/OnDemand/OnDemand';
@@ -19,4 +46,4 @@ const AppRoutes = () => {
     );
 };
 
-export default AppRoutes;
+export default AppRoutes; */
